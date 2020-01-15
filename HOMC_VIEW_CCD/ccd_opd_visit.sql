180dfc94f48615e3bb4edc69ffc65a90a9e8e721
Thanikul Sriuthis <moremeng@dv4.biz>
2020-01-15 19:16:50 +0700

    ใช้ข้อมูลเท่าที่จำเป็น หลังเปิดคลินิกเท่านั้น

--- HOMC_VIEW_CCD/ccd_opd_visit.sql
+++ HOMC_VIEW_CCD/ccd_opd_visit.sql
@@ -1,22 +1,26 @@
 ALTER VIEW [dbo].[ccd_opd_visit]
 AS
-SELECT DISTINCT ctl.PCUCODE AS hospcode,
-    LTRIM(o.hn) AS hn,
-    o.regNo AS vn,
+SELECT DISTINCT CTL.PCUCODE AS hospcode,
+    LTRIM(O.hn) AS hn,
+    O.regNo AS vn,
     I.ladmit_n AS an,
-    (convert(DATETIME, convert(CHAR, regist_date - 5430000))) AS vstdate,
-    convert(TIME, (left(o.timePt, 2) + ':' + right(o.timePt, 2)), 108) AS vsttime,
-    isnull(RTRIM(dbo.DEPTMAP(d.deptCode, 'OPPP')), '00100') AS clinic_code,
+    -- CONVERT(DATE, CONVERT(CHAR(8), (O.registDate - 10860000))) AS vstdate,
+    (CONVERT(DATE, CONVERT(CHAR(8), O.registDate - 5430000))) AS vstdate,
+    CONVERT(TIME, LEFT(O.timePt, 2) + ':' + SUBSTRING(O.timePt, 3, 2) + ':00') AS vsttime,
+    ISNULL(RTRIM(dbo.DEPTMAP(Q.deptCode, 'OPPP')), '00100') AS clinic_code,
     CASE
-        WHEN (dbo.PAYTYPEMAP(b.useDrg, 'OPPP') = NULL) OR (dbo.PAYTYPEMAP(b.useDrg, 'OPPP') = '')
+        WHEN (dbo.PAYTYPEMAP(B.useDrg, 'OPPP') = NULL) OR (dbo.PAYTYPEMAP(B.useDrg, 'OPPP') = '')
             THEN '9100'
-        ELSE isnull(dbo.PAYTYPEMAP(b.useDrg, 'OPPP'), '9100')
+        ELSE ISNULL(RTRIM(dbo.PAYTYPEMAP(B.useDrg, 'OPPP')), '9100')
         END AS pttype_std_code,
-    DEPT.deptDesc AS name,
-    '' AS pttype_std_name
-FROM dbo.OPD_H AS o(NOLOCK)
-LEFT JOIN Ipd_h I(NOLOCK) ON I.hn = o.hn AND I.regist_flag = o.regNo
-INNER JOIN Deptq_d d(NOLOCK) ON o.hn = d.hn AND o.regNo = d.regNo
-INNER JOIN DEPT(NOLOCK) ON d.deptCode = DEPT.deptCode
-INNER JOIN Bill_h AS b(NOLOCK) ON b.hn = o.hn AND b.regNo = o.regNo
-LEFT JOIN PPOP_CON AS ctl ON ctl.CON_KEY = '000'
+    D.deptDesc AS name,
+    PT.pay_typedes AS pttype_std_name
+FROM dbo.OPD_H O(NOLOCK)
+LEFT JOIN dbo.Ipd_h I(NOLOCK) ON I.hn = O.hn AND I.regist_flag = O.regNo
+INNER JOIN dbo.Deptq_d Q(NOLOCK) ON O.hn = Q.hn AND O.regNo = Q.regNo
+INNER JOIN dbo.DEPT D(NOLOCK) ON Q.deptCode = D.deptCode
+INNER JOIN dbo.Bill_h B(NOLOCK) ON B.hn = O.hn AND B.regNo = O.regNo
+LEFT JOIN dbo.Paytype PT WITH (NOLOCK) ON PT.pay_typecode = B.useDrg
+LEFT JOIN dbo.PPOP_CON CTL ON CTL.CON_KEY = '000'
+WHERE O.registDate > '25621001'
+-- AND LTRIM(O.hn) = '81247'
