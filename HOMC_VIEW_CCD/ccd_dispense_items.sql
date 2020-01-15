cc3bba709b1df8e7af6b932f3a86f723a67dcfe0
Thanikul Sriuthis <moremeng@dv4.biz>
2020-01-15 14:57:53 +0700

    ปรับ view ใหม่ โดยดูของ กระบี่เพื่อเปรียบเทียบข้อมูล

--- HOMC_VIEW_CCD/ccd_dispense_items.sql
+++ HOMC_VIEW_CCD/ccd_dispense_items.sql
@@ -1,49 +1,25 @@
-CREATE VIEW [dbo].[ccd_dispense_items]
+ALTER VIEW [dbo].[ccd_dispense_items]
 AS
-SELECT DISTINCT ctl.PCUCODE AS hospcode,
+SELECT TOP 100 ctl.PCUCODE AS hospcode,
 	LTRIM(p.hn) AS hn,
 	p.registNo AS vn,
 	p.invCode AS hosp_drug_code,
 	M.name AS drug_name,
-	rtrim(L1.lamed_name) + '  ' + rtrim(lamedQty) + '  ' + rtrim(L2.lamed_name) + '  ' + rtrim(lamedTimeText) AS drug_usage,
-	(CONVERT(MONEY, isnull(p.accQty, 0))) AS qty,
-	rtrim(L2.lamed_name) AS unit,
-	CONVERT(MONEY, isnull(M.opd_prc, 0)) AS unit_price,
-	(CONVERT(MONEY, isnull(p.accAmt, 0))) AS sum_price,
+	RTRIM(L1.lamed_name) + '  ' + RTRIM(p.lamedQty) + '  ' + RTRIM(L2.lamed_name) + '  ' + RTRIM(p.lamedTimeText) AS drug_usage,
+	ISNULL(p.accQty, 0) AS qty,
+	RTRIM(M.package) AS unit,
+	CONVERT(MONEY, ISNULL(M.opd_prc, 0)) AS unit_price,
+	(CONVERT(MONEY, ISNULL(p.accAmt, 0))) AS sum_price,
 	LEFT(ISNULL(g.invGovCode24, M.code), 24) AS didstd,
 	dc.TMTID AS tmtcode
-FROM dbo.OPD_H AS o(NOLOCK)
-INNER JOIN Patmed p WITH (NOLOCK) ON o.hn = p.hn
-	AND o.regNo = p.registNo
-LEFT JOIN Med_inv M WITH (NOLOCK) ON p.invCode = M.code
-	AND M.site = '1'
-LEFT JOIN Lamed L1 WITH (NOLOCK) ON (
-		(L1.lamed_code = '*' + p.lamedHow)
-		OR (L1.lamed_code = p.lamedHow)
-		)
-	AND (
-		p.lamedHow <> '*.'
-		OR p.lamedHow IS NOT NULL
-		)
-LEFT JOIN Lamed L2 WITH (NOLOCK) ON (L2.lamed_code = '!' + p.lamedUnit)
-	OR (L2.lamed_code = p.lamedUnit)
-	AND (
-		p.lamedUnit <> '!.'
-		OR p.lamedUnit IS NOT NULL
-		)
-LEFT JOIN Lamed L3 WITH (NOLOCK) ON (L3.lamed_code = '&' + p.lamedTime)
-	OR (L3.lamed_code = p.lamedTime)
-	AND (
-		p.lamedTime <> '&.'
-		OR p.lamedTime IS NOT NULL
-		)
-LEFT JOIN Lamed L4 WITH (NOLOCK) ON (L4.lamed_code = '@' + p.lamedSpecial)
-	OR (L4.lamed_code = p.lamedSpecial)
-	AND (
-		p.lamedSpecial <> '@.'
-		OR p.lamedSpecial IS NOT NULL
-		)
-LEFT JOIN dbo.Med_Gov24Map AS g(NOLOCK) ON g.invCode = M.code
-	AND g.invType = 'M'
-LEFT JOIN Drugcatalog_producttype dc(NOLOCK) ON dc.hospDrugCode = M.code
-LEFT JOIN dbo.PPOP_CON AS ctl(NOLOCK) ON ctl.CON_KEY = '000'
+FROM dbo.Patmed AS p
+LEFT OUTER JOIN dbo.OPD_H AS o WITH (NOLOCK) ON p.hn = o.hn AND p.registNo = o.regNo
+LEFT OUTER JOIN dbo.Med_inv M WITH (NOLOCK) ON p.invCode = M.code AND M.site = '1'
+LEFT OUTER JOIN dbo.Drugcatalog_producttype dc WITH (NOLOCK) ON dc.hospDrugCode = M.code
+LEFT OUTER JOIN dbo.Lamed L1 WITH (NOLOCK) ON L1.lamed_code = p.lamedHow
+LEFT OUTER JOIN dbo.Lamed L2 WITH (NOLOCK) ON L2.lamed_code = p.lamedUnit
+LEFT OUTER JOIN dbo.Med_Gov24Map AS g WITH (NOLOCK) ON g.invCode = M.code AND g.invType = 'M'
+LEFT OUTER JOIN dbo.PPOP_CON AS ctl WITH (NOLOCK) ON ctl.CON_KEY = '000'
+-- WHERE p.invCode = 'THCO1'
+-- WHERE dc.TMTID LIKE '1129720'
+
