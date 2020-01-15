f04f26a0d3ebc69f36e57b8ff4916db9c4ad8646
Thanikul Sriuthis <moremeng@dv4.biz>
2020-01-15 20:21:24 +0700

    ปรับให้แสดงเฉพาะ OPD เท่านั้น และสำหรับ visit หลัง ต.ค.62

--- HOMC_VIEW_CCD/ccd_opd_visit_diag.sql
+++ HOMC_VIEW_CCD/ccd_opd_visit_diag.sql
@@ -1,17 +1,17 @@
-CREATE VIEW [dbo].[ccd_opd_visit_diag]
+ALTER VIEW [dbo].[ccd_opd_visit_diag]
 AS
-SELECT ctl.PCUCODE AS hospcode,
+SELECT DISTINCT ctl.PCUCODE AS hospcode,
 	LTRIM(o.hn) AS hn,
 	o.regNo AS vn,
 	d.ICDCode AS icd10,
-	ICD101.DES AS diagname,
+	ICD101.DES AS diag_name,
 	CASE
 		WHEN DiagType = 'E'
 			THEN '5'
 		ELSE d.dxtype
 		END AS diag_type,
-	d.DocCode,
-	dc.CID
+	-- d.DocCode,
+	dc.CID AS doctor_cid
 FROM dbo.OPD_H AS o(NOLOCK)
 INNER JOIN dbo.PATDIAG AS d(NOLOCK) ON d.Hn = o.hn
 	AND d.regNo = o.regNo
@@ -20,3 +20,7 @@ INNER JOIN dbo.PATDIAG AS d(NOLOCK) ON d.Hn = o.hn
 INNER JOIN ICD101(NOLOCK) ON ICD101.CODE = d.ICDCode
 INNER JOIN PROVIDER AS dc(NOLOCK) ON dc.PROVIDER = d.DocCode
 LEFT JOIN dbo.PPOP_CON AS ctl(NOLOCK) ON ctl.CON_KEY = '000'
+WHERE
+o.ipdStatus = '0' AND o.registDate > '25621001'
+-- AND LTRIM(o.hn) = '18972'
+-- AND o.registDate = '25621202'
