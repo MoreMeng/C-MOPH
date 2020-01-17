14fc2fae75be9b0008c72535faafb7a548b62eae
Thanikul Sriuthis <moremeng@dv4.biz>
2020-01-17 20:48:06 +0700

    ปัญหาเกิดจาก C-MOPH เอง ประสานงานให้ตรวจสอบแล้ว

--- HOMC_VIEW_CCD/ccd_opd_visit_procedure.sql
+++ HOMC_VIEW_CCD/ccd_opd_visit_procedure.sql
@@ -1,17 +1,26 @@
-CREATE VIEW [dbo].[ccd_opd_visit_procedure]
+ALTER VIEW [dbo].[ccd_opd_visit_procedure]
 AS
-SELECT ctl.PCUCODE AS hospcode,
+SELECT TOP 100 ctl.PCUCODE AS hospcode,
+    -- o.registDate,
 	LTRIM(o.hn) AS hn,
 	o.regNo AS vn,
-	d.ICDCode AS icdcm,
+	RTRIM(d.ICDCode) AS icdcm,
+	-- RTRIM(d.ICDCode) AS icd,
+	-- RTRIM(d.ICDCode) AS icd10,
+	-- ICDCM1.DES AS diag_name,
 	ICDCM1.DES AS proced_name,
-	'' proced_type,
-	dc.CID
+	-- d.DiagNo AS diag_type,
+	d.DiagNo AS proced_type,
+	dc.CID AS doctor_cid
 FROM dbo.OPD_H AS o(NOLOCK)
 INNER JOIN dbo.PATDIAG AS d(NOLOCK) ON d.Hn = o.hn
 	AND d.regNo = o.regNo
 	AND d.DiagType = 'P'
-	AND dxtype IN ('1', '4')
-INNER JOIN ICDCM1(NOLOCK) ON ICDCM1.CODE = d.ICDCode
-INNER JOIN PROVIDER AS dc(NOLOCK) ON dc.PROVIDER = d.DocCode
-LEFT JOIN dbo.PPOP_CON AS ctl(NOLOCK) ON ctl.CON_KEY = '000'
+	AND d.dxtype IN ('1', '4')
+INNER JOIN dbo.ICDCM1(NOLOCK) ON ICDCM1.CODE = d.ICDCode
+INNER JOIN dbo.PROVIDER dc(NOLOCK) ON dc.PROVIDER = d.DocCode
+LEFT JOIN dbo.PPOP_CON ctl(NOLOCK) ON ctl.CON_KEY = '000'
+WHERE
+o.registDate >= '25621001'
+AND (d.ICDCode IS NOT NULL) AND (d.ICDCode <> '') AND (d.DiagType = 'P')
+-- AND LTRIM(o.hn) IN ('321420','18972', '202042' ,'282876','81247')
