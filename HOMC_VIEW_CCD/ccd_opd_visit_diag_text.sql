cc4155e80ec8103b9cc465ff1a11ced74f39d5aa
Thanikul Sriuthis <moremeng@dv4.biz>
2020-01-17 20:47:20 +0700

    เพิ่มเงื่อนไข

--- HOMC_VIEW_CCD/ccd_opd_visit_diag_text.sql
+++ HOMC_VIEW_CCD/ccd_opd_visit_diag_text.sql
@@ -14,5 +14,6 @@ INNER JOIN dbo.PATDIAG AS d(NOLOCK) ON d.Hn = o.hn AND d.regNo = o.regNo AND d.D
 LEFT JOIN dbo.PatSS(NOLOCK) AS s ON s.hn = o.hn
 LEFT JOIN dbo.PPOP_CON AS ctl(NOLOCK) ON ctl.CON_KEY = '000'
 WHERE o.ipdStatus = '0' AND o.registDate >= '25621001'
+AND (d.ICDCode IS NOT NULL) AND (d.ICDCode <> '') AND (d.DiagType = 'P')
 -- AND LTRIM(o.hn) = '18972'
     -- AND o.registDate = '25621202'
